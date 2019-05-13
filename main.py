2e9df3469bad557c0e1bd407def9413d1f19e7d5
laertides <odysseassu@gmail.com>
2019-05-14 00:49:37 +0300

    removed stego

--- main.py
+++ main.py
@@ -8,7 +8,6 @@ from Crypto.PublicKey import RSA
 from Crypto.Cipher import AES, PKCS1_OAEP
 from Crypto.Random import get_random_bytes
 from tkinter.filedialog import askopenfilename
-from cryptosteganography import CryptoSteganography
 
 root = tk.Tk()
 root.withdraw()
